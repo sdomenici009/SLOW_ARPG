65d92310f21524bf969960eae4b73f0ea39bb90b
Stephen Domenici <sdomenici009@gmail.com>
2018-07-07 18:30:53 -0700

    Temporary classes added for initial prototype.

---
+++ Assets/_Scripts/MathUtils.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public static class MathUtils {
+
+	private const int PIXELS_PER_UNIT = 64;
+
+	public static Vector3 RoundPlayerPositionToPixelPerfect (Vector3 position) {
+		float x = Mathf.Round (position.x * PIXELS_PER_UNIT) / PIXELS_PER_UNIT;
+		float y = Mathf.Round (position.y * PIXELS_PER_UNIT) / PIXELS_PER_UNIT;
+		return new Vector3 (x, y, 0);
+	}
+}
